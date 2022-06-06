bfe4bd09e2caa6fbf15aa9de58f90539e2269e7a
IsDerky <alex.miguel.1805@gmail.com>
2022-06-06 16:20:00 +0200

    asd

--- pages/index.js
+++ pages/index.js
@@ -118,7 +118,7 @@ const Home = () => (
         </Paragraph>
         <Link href="https://spotify-github-profile.vercel.app/api/view?uid=darkqwew&redirect=true">
        <>
-        <WorkImage
+        <Image
       src="https://spotify-github-profile.vercel.app/api/view?uid=darkqwew&cover_image=true&theme=novatorem&bar_color=202023&bar_color_cover=falsea"
       alt="Spotify"
       layout="responsive"
