faeadee3cfc84e9a5304dbc543c8e1e102a84661
IsDerky <alex.miguel.1805@gmail.com>
2022-06-06 16:12:42 +0200

    sd

--- pages/index.js
+++ pages/index.js
@@ -116,14 +116,17 @@ const Home = () => (
             Streams
           </Link>
         </Paragraph>
+        <Link href="https://spotify-github-profile.vercel.app/api/view?uid=darkqwew&redirect=true">
+       <a>
         <WorkImage
       src="https://spotify-github-profile.vercel.app/api/view?uid=darkqwew&cover_image=true&theme=novatorem&bar_color=202023&bar_color_cover=falsea"
       alt="Spotify"
       layout="responsive"
       width="100"
       height="50"
-      href="https://spotify-github-profile.vercel.app/api/view?uid=darkqwew&redirect=true"
     />
+    </a>
+    </Link>
       </Section>
 
       <Section delay={0.3}>
