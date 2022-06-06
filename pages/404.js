ae85c7cb178948de7f34eeb915a9598da878ec48
IsDerky <alex.miguel.1805@gmail.com>
2022-06-06 16:39:03 +0200

    Last

--- pages/404.js
+++ pages/404.js
@@ -9,7 +9,7 @@ const NotFound = () => {
       <Divider my={6} />
       <Box my={6} align="center">
         <NextLink href="/">
-          <Button colorScheme="teal">Return to home</Button>
+          <Button colorScheme="teal">Return home</Button>
         </NextLink>
       </Box>
     </Container>
