329b1f2745395dddbbc039fdad1a768f265dbf51
IsDerky <alex.miguel.1805@gmail.com>
2022-06-06 02:02:25 +0200

    asd

--- components/footer.js
+++ components/footer.js
@@ -5,7 +5,12 @@ const Footer = () => {
     <Box align="center" opacity={0.4} fontSize="sm">
       &copy; {new Date().getFullYear()} Derkyu. All Rights Reserved.
 
-      https://spotify-github-profile.vercel.app/api/view?uid=darkqwew&cover_image=false&theme=natemoo-re&bar_color=53b14f&bar_color_cover=false
+      <Image
+      src="https://spotify-github-profile.vercel.app/api/view?uid=darkqwew&cover_image=false&theme=natemoo-re&bar_color=53b14f&bar_color_cover=false"
+      alt="Picture of the author"
+      width="150px"
+      height="100px"
+    />
     </Box>
     
   )
