24d523c605d0f048cd0978d37e046ebe8a48c65d
IsDerky <alex.miguel.1805@gmail.com>
2022-06-06 02:23:27 +0200

    asd

--- components/footer.js
+++ components/footer.js
@@ -7,9 +7,10 @@ const Footer = () => {
     
       <WorkImage
       src="https://spotify-github-profile.vercel.app/api/view?uid=darkqwew&cover_image=false&theme=natemoo-re&bar_color=53b14f&bar_color_cover=false"
-      alt="Picture of the author"
-      width="350px"
-      height="300px"
+      alt="Spotify"
+      width="150px"
+      height="100px"
+      align="left"
     />
     
     </Box>
