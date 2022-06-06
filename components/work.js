40470e0a8144b49b31dfa4e3af1275462b27f31f
IsDerky <alex.miguel.1805@gmail.com>
2022-06-06 16:26:35 +0200

    asd

--- components/work.js
+++ components/work.js
@@ -21,6 +21,10 @@ export const WorkImage = ({ src, alt }) => (
   <Image borderRadius="lg" w="full" src={src} alt={alt} mb={4} />
 )
 
+export const SpotifyAPP = ({ src, alt }) => (
+  <Image borderRadius="lg" w="full" src={src} alt={alt} mb={4} />
+)
+
 export const Meta = ({ children }) => (
   <Badge colorScheme="green" mr={2}>
     {children}
