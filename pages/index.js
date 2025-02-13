cf4eb63f8beeb4c9c8c47093985ffc8693023a67
Derkyu <alex.miguel.1805@gmail.com>
2025-02-13 22:12:29 +0100

    Add animation to Heading

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ import { IoLogoGithub } from 'react-icons/io5'
 import { FaTwitch, FaDiscord } from "react-icons/fa"
 import Image from 'next/image'
 import { SpotifyAPP } from '../components/work'
+import SplitText from '../components/SplitText'
 
 const ProfileImage = chakra(Image, {
   shouldForwardProp: prop => ['width', 'height', 'src', 'alt'].includes(prop)
@@ -33,7 +34,16 @@ const Home = () => {
         <Box display={{ md: 'flex' }}>
           <Box flexGrow={1}>
             <Heading as="h2" variant="page-title">
-              Derkyu
+            <SplitText
+            text="Derkyu"
+            className="text-2xl font-semibold text-center"
+            delay={150}
+            animationFrom={{ opacity: 0, transform: 'translate3d(0,-50px,0)' }}
+            animationTo={{ opacity: 1, transform: 'translate3d(0,0,0)' }}
+            easing="easeOutCubic"
+            threshold={0.1}
+            rootMargin="-50px"
+            />
             </Heading>
             <p>Digital Enjoyer (Gamer / Developer / Chatter)</p>
           </Box>
